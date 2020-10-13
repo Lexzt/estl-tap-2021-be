2d1fde5dba0cb071682cd1b6de8a68863ac7b371
Lexzt <fongzhizhong@gmail.com>
2020-10-13 17:08:10 +0800

    patch feature done

--- src/utils/crudUser.js
+++ src/utils/crudUser.js
@@ -6,11 +6,8 @@ const regexAlphaNumeric = RegExp(/^[a-z0-9]+$/i);
 const regexNumeric = RegExp(/^\-?[0-9]+(e[0-9]+)?(\.[0-9]+)?$/);
 
 async function postUser(req, res) {
-  if (req.params.id !== req.body.id) {
-    return res.status(500).json({ message: "Different ID" });
-  }
-
   // Check if ID is valid
+  const { id } = req.params;
   const { id, login, name, salary } = req.body;
 
   let salaryCheck;
@@ -80,25 +77,90 @@ async function postUser(req, res) {
 }
 
 async function patchUser(req, res) {
-  //   console.log(req.params);
-  //   const { id } = req.params;
-  //   if (!id || !regexAlphaNumeric.test(id)) {
-  //     return res.status(400).json({ message: "Missing Id" });
-  //   }
-  //   const dbResults = await users.findOne({
-  //     where: {
-  //       id: {
-  //         [Op.eq]: id,
-  //       },
-  //     },
-  //   });
-  //   const results = {
-  //     id: dbResults.id,
-  //     name: dbResults.name,
-  //     login: dbResults.login,
-  //     salary: dbResults.salary,
-  //   };
-  //   return res.status(200).json(results);
+  // Check if ID is valid
+  const { id } = req.params;
+  const { login, name, salary } = req.body;
+
+  let salaryCheck;
+  if (typeof salary === "string") {
+    salaryCheck = parseFloat(salary.trim()) >= 0;
+  } else {
+    salaryCheck = salary >= 0;
+  }
+
+  if (
+    !(
+      regexAlphaNumeric.test(id.trim()) &&
+      regexAlphaNumeric.test(login.trim()) &&
+      regexAlphaNumeric.test(name.trim()) &&
+      regexNumeric.test(salary) &&
+      salaryCheck
+    )
+  ) {
+    return res.status(500).json({ message: "Invalid Body" });
+  }
+
+  const t = await database.transaction();
+
+  try {
+    // Check if ID exist in DB
+    const dbResults = await users.findAll(
+      {
+        where: {
+          id: {
+            [Op.eq]: id,
+          },
+        },
+      },
+      { transaction: t }
+    );
+    if (dbResults.length > 0) {
+      return res.status(400).json({ message: "Invalid ID, Ignoring" });
+    }
+
+    // Check if Name exist in DB
+    const dbResults = await users.findAll(
+      {
+        where: {
+          login: {
+            [Op.eq]: login,
+          },
+        },
+      },
+      { transaction: t }
+    );
+
+    if (dbResults.length > 0) {
+      return res.status(400).json({ message: "Overlapping Login, Ignoring" });
+    }
+
+    const updateResult = await users.update(
+      {
+        id: id,
+        login: login,
+        name: name,
+        salary: salary,
+      },
+      {
+        where: {
+          id: id,
+        },
+        transaction: t,
+      }
+    );
+    const results = {
+      id: updateResult.id,
+      name: updateResult.name,
+      login: updateResult.login,
+      salary: updateResult.salary,
+    };
+    await t.commit();
+    return res.status(200).json(results);
+  } catch (error) {
+    console.log(error);
+    await t.rollback();
+    return res.status(500).json({ message: error });
+  }
 }
 
 async function getUser(req, res) {
